b053c07bec6305983e1d0da079052e427e7d7615
Sara Robles <sararoblesbarbosa2@gmail.com>
2025-05-19 15:20:19 -0300

    add directory structure

---
+++ src/main/java/br/edu/fatecgru/buscameds/BuscamedsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecgru.buscameds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscamedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscamedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fatecgru/buscameds/BuscamedsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecgru.buscameds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscamedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
