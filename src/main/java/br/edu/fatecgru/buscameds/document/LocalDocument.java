d11bcdec6795530c5dd847ec42589ba74893b399
Sara Robles <sararoblesbarbosa2@gmail.com>
2025-05-24 14:56:43 -0300

    alter documents fields

--- src/main/java/br/edu/fatecgru/buscameds/document/LocalDocument.java
+++ src/main/java/br/edu/fatecgru/buscameds/document/LocalDocument.java
@@ -1,21 +1,60 @@
 package br.edu.fatecgru.buscameds.document;
 
-import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
-@ToString(exclude = "cnes")
-@AllArgsConstructor
-@NoArgsConstructor
 @Document(collection = "locals")
 public class LocalDocument {
 
     @Id
     private String cnes;
-    private String address;
-    private Integer telephone;
+    private Address address;
+    private String phone;
     private String name;
 
+    public LocalDocument() {}
+
+    public LocalDocument(String cnes, Address address, String phone, String name) {
+        this.cnes = cnes;
+        this.address = address;
+        this.phone = phone;
+        this.name = name;
+    }
+
+    public String getCnes() {
+        return cnes;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "LocalDocument{" +
+                "address=" + address +
+                ", phone='" + phone + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/br/edu/fatecgru/buscameds/document/MedicineDocument.java
+++ src/main/java/br/edu/fatecgru/buscameds/document/MedicineDocument.java
@@ -1,18 +1,30 @@
 package br.edu.fatecgru.buscameds.document;
 
-import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
-@ToString(exclude = "catmatCode")
-@AllArgsConstructor
-@NoArgsConstructor
 @Document(collection = "medicines")
 public class MedicineDocument {
     @Id
     private String catmatCode;
     private String name;
 
+    public MedicineDocument() {}
+
+    public MedicineDocument(String catmatCode, String name) {
+        this.catmatCode = catmatCode;
+        this.name = name;
+    }
+
+    public String getCatmatCode() {
+        return catmatCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
