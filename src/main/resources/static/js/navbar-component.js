bc95c29cb0b160b9d541b23c3ab9fb472ec4d59f
Sara Robles <sararoblesbarbosa2@gmail.com>
2025-06-05 20:02:55 -0300

    fix frontend

--- src/main/resources/static/js/navbar-component.js
+++ src/main/resources/static/js/navbar-component.js
@@ -1,34 +1,46 @@
 class NavbarComponent extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" 
-                            href="/buscameds/home">
-                                Home</a>
-                        <a class="nav-link" 
-                            href="/buscameds/favorites">
-                                Favoritos</a>
-                        <a class="nav-link" 
-                            href="/buscameds/user/login" 
-                            id="loginLink">
-                                Login</a>
-                        <a class="nav-link d-none" 
-                            href="/buscameds/user/update"
-                            id="updatePageButton">
-                                Alterar Conta</button>
-                        <a class="nav-link d-none" 
-                            href=""
-                            id="logoutButton">
-                                Logout</button>
+            <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavAltMarkup">
+
+                        <!-- Itens à esquerda -->
+                        <div class="navbar-nav">
+                            <a class="nav-link text-white" href="/buscameds/home">Home</a>
+                            <a class="nav-link text-white" href="/buscameds/favorites">Favoritos</a>
+                        </div>
+
+                        <!-- Dropdown "Conta" à direita -->
+                        <div class="navbar-nav">
+                            <div class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-white" href="#" id="accountDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-person-circle"></i> Conta
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end bg-primary text-white" aria-labelledby="accountDropdown">
+                                    <li>
+                                        <a class="dropdown-item text-white" href="/buscameds/user/login" id="loginLink">
+                                            <i class="bi bi-box-arrow-in-right"></i> Login
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item text-white d-none" href="#" id="logoutButton">
+                                            <i class="bi bi-box-arrow-right"></i> Logout
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item text-white d-none" href="/buscameds/user/update" id="updatePageButton">
+                                            <i class="bi bi-gear"></i> Alterar Conta
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
         `;
     }
 }
-
 customElements.define('navbar-component', NavbarComponent);
